f00236e3425bc6d8e9ab6ac686ea93a9f74c79d0
Bram <bramjanssen@hey.com>
2026-09-24 17:21:38 +0200

    Remove unused right-click menu override

--- nvim/plugin/menu.lua
+++
@@ -1,8 +0,0 @@
--- Override right click mouse file
-
-vim.cmd [[
-  aunmenu PopUp
-  anoremenu PopUp.Inspect     <cmd>Inspect<CR>
-  amenu PopUp.-1-             <NOP>
-  nnoremenu PopUp.Back        <C-t>
-]]
